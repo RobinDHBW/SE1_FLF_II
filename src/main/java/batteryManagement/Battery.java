0cb1a6274c22253febc67711060f66120bbe4331
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2022-03-06 20:42:10 +0100

    finish task3

--- src/main/java/batteryManagement/Battery.java
+++ src/main/java/batteryManagement/Battery.java
@@ -1,13 +1,48 @@
 package batteryManagement;
 
+import configuration.Configuration;
 import store.StoreMedium;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Battery extends StoreMedium {
+    private List<MainCell> mainCells;
 
     public Battery(Object subject, Integer length, Integer height, Integer width) {
         super(length, height, width, subject);
+
+        //Instanziierung Topdown
+        List<MainCell> topLevelCells = new ArrayList<>();
+        List<SubCell> midLevelCells = new ArrayList<>();
+        List<Cell> lowLevelCells = new ArrayList<>();
+
+        for (int i = 0; i < Configuration.instance.mainCellPerBatterie; i++) {
+            topLevelCells.add(new MainCell());
+            for (int j = 0; j < Configuration.instance.subCellPerMainCell; j++) {
+                midLevelCells.add(new SubCell());
+                for (int k = 0; k < Configuration.instance.cellPerSubCell; k++) {
+                    lowLevelCells.add(new Cell());
+                }
+            }
+        }
+
+        //Zusammensetzung Bottom up
+        int i = 0;
+        int j = 0;
+        for (MainCell m : topLevelCells) {
+            int k = 0;
+            for (SubCell s : midLevelCells) {
+                for (int z = 0; z < 3; z++) {
+                    s.addCell(lowLevelCells.get(k++));
+                }
+            }
+            for (int z = 0; z < 4; z++) {
+                m.addCell(midLevelCells.get(i++));
+            }
+            this.mainCells.add(topLevelCells.get(j++));
+        }
+
     }
 
     @Override
--- src/main/java/configuration/Configuration.java
+++ src/main/java/configuration/Configuration.java
@@ -5,4 +5,8 @@ public enum Configuration {
     public final String flfIdentifier = "DUS | FLF-5";
     public final String cuIdent = "FT-DUS-FLF-5";
     public final String cuCode = "6072";
+
+    public final Integer cellPerSubCell = 10;
+    public final Integer subCellPerMainCell = 100;
+    public final Integer mainCellPerBatterie = 100;
 }
