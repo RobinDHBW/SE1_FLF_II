206619e39206197f9abd516e9a26d53d124e4b6d
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2022-03-06 20:04:22 +0100

    task3

--- src/main/java/batteryManagement/Cell.java
+++ src/main/java/batteryManagement/Cell.java
@@ -1,4 +1,21 @@
 package batteryManagement;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cell {
+    protected List<Cell> cells;
+
+    public Cell(){
+        this.cells = new ArrayList<>();
+    }
+
+    public void addCell(Cell c){
+        cells.add(c);
+    }
+
+    public Boolean isComposite(){
+        return !cells.isEmpty();
+    }
+
 }
