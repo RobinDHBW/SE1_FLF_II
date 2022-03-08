76401f6a20807d856b8467ca385fd248ebadd384
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2022-03-08 23:38:18 +0100

    2/3 working

--- src/main/java/task4/CryptoStrategyRSA.java
+++ src/main/java/task4/CryptoStrategyRSA.java
@@ -1,14 +1,57 @@
 package task4;
 
+import javax.crypto.Cipher;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
+
 public class CryptoStrategyRSA implements ICryptoStrategy {
+    private final PrivateKey privateKey;
+    private final PublicKey publicKey;
+    public CryptoStrategyRSA(){
+        KeyPair pair = this.generateKeys();
+        this.privateKey = pair.getPrivate();
+        this.publicKey = pair.getPublic();
+    }
+
+    private KeyPair generateKeys() {
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(1024);
+            return  keyGen.generateKeyPair();
+        }catch (Exception ex){
+            System.err.println(ex.getMessage());
+            ex.printStackTrace();
+            return null;
+        }
+    }
 
     @Override
     public String encrypt(String plain, String key) {
-        return null;
+        try{
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, this.publicKey);
+            return cipher.doFinal(plain.getBytes()).toString();
+        }catch (Exception ex){
+            System.err.println(ex.getMessage());
+            ex.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public String decrypt(String cipher, String key) {
-        return null;
+        try{
+            Cipher cipherInstance = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipherInstance.init(Cipher.DECRYPT_MODE, privateKey);
+            return new String(cipherInstance.doFinal(cipher.getBytes(StandardCharsets.UTF_8)));
+        }catch (Exception ex){
+            System.err.println(ex.getMessage());
+            ex.printStackTrace();
+            return null;
+        }
     }
 }
---
+++ src/test/java/testTask4/TestTask4.java
@@ -0,0 +1,35 @@
+package testTask4;
+
+import configuration.Configuration;
+import org.junit.jupiter.api.Test;
+
+import task4.CryptoStrategyAES;
+import task4.CryptoStrategyDES;
+import task4.CryptoStrategyRSA;
+import task4.ICryptoStrategy;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestTask4 {
+    private ICryptoStrategy cryptoStrategy;
+    private String test ="The quick brown fox jumps over the lazy dog";
+    private String cuCode = Configuration.instance.cuCode;
+
+    @Test
+    void testAESStrategy(){
+        this.cryptoStrategy = new CryptoStrategyAES(Configuration.instance.cuSalt);
+        assertEquals(test,cryptoStrategy.decrypt(cryptoStrategy.encrypt(test, cuCode), cuCode));
+    }
+
+    @Test
+    void testRSAStrategy(){
+        this.cryptoStrategy = new CryptoStrategyRSA();
+        assertEquals(test,cryptoStrategy.decrypt(cryptoStrategy.encrypt(test, cuCode), cuCode));
+    }
+
+    @Test
+    void testDESStrategy(){
+        this.cryptoStrategy = new CryptoStrategyDES();
+        assertEquals(test,cryptoStrategy.decrypt(cryptoStrategy.encrypt(test, cuCode), cuCode));
+    }
+}
