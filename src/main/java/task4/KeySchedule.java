8faec9a286f9db726795408413ec2de7e121746d
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2022-03-12 11:48:54 +0100

    apply feedback

--- src/main/java/task4/KeySchedule.java
+++ src/main/java/task4/KeySchedule.java
@@ -1,31 +1,38 @@
 package task4;
 
 public class KeySchedule {
-    private final int[][] firstPermutionMatrix = {
-            {57, 49, 41, 33, 25, 17, 9, 1},
-            {58, 50, 42, 34, 26, 18, 10, 2},
-            {59, 51, 43, 35, 27, 19, 11, 3},
-            {60, 52, 44, 36, 63, 55, 47, 39},
-            {31, 23, 15, 7, 62, 54, 46, 38},
-            {30, 22, 14, 6, 61, 53, 45, 37},
-            {29, 21, 13, 5, 28, 20, 12, 4}
-    };
+    private final int[][] firstPermutationMatrix;
 
-    private final int[][] secondPermutionMatrix = {
-            {14, 17, 11, 24, 1, 5, 3, 28},
-            {15, 6, 21, 10, 23, 19, 12, 4},
-            {26, 8, 16, 7, 27, 20, 13, 2},
-            {41, 52, 31, 37, 47, 55, 30, 40},
-            {51, 45, 33, 48, 44, 49, 39, 56},
-            {34, 53, 46, 42, 50, 36, 29, 32}
-    };
+    private final int[][] secondPermutationMatrix;
+
+    public KeySchedule() {
+        this.firstPermutationMatrix = new int[][]{
+                {57, 49, 41, 33, 25, 17, 9, 1},
+                {58, 50, 42, 34, 26, 18, 10, 2},
+                {59, 51, 43, 35, 27, 19, 11, 3},
+                {60, 52, 44, 36, 63, 55, 47, 39},
+                {31, 23, 15, 7, 62, 54, 46, 38},
+                {30, 22, 14, 6, 61, 53, 45, 37},
+                {29, 21, 13, 5, 28, 20, 12, 4}
+        };
+
+        this.secondPermutationMatrix = new int[][]{
+                {14, 17, 11, 24, 1, 5, 3, 28},
+                {15, 6, 21, 10, 23, 19, 12, 4},
+                {26, 8, 16, 7, 27, 20, 13, 2},
+                {41, 52, 31, 37, 47, 55, 30, 40},
+                {51, 45, 33, 48, 44, 49, 39, 56},
+                {34, 53, 46, 42, 50, 36, 29, 32}
+        };
+
+    }
 
 
     public String firstPerm(String input) {
         char[] result = new char[56];
 
         for (int i = 0; i < result.length; i++) {
-            result[i] = input.charAt(firstPermutionMatrix[i / 8][i % 8] - 1);
+            result[i] = input.charAt(firstPermutationMatrix[i / 8][i % 8] - 1);
         }
 
         return new String(result);
@@ -35,7 +42,7 @@ public class KeySchedule {
         char[] result = new char[48];
 
         for (int i = 0; i < 48; i++) {
-            result[i] = input.charAt(secondPermutionMatrix[i / 8][i % 8] - 1);
+            result[i] = input.charAt(secondPermutationMatrix[i / 8][i % 8] - 1);
         }
 
         return new String(result);
